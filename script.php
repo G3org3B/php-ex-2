50bb83c27c8ddff35dc4a3a0e881ff4c6d579f8c
G3org3B <you@example.com>
2019-01-23 20:59:26 +0100

    complete first exercise

--- script.php
+++ script.php
@@ -5,14 +5,27 @@
     <title></title>
   </head>
   <body>
+
     <?php
 
-    echo "Ciao";
+      $password = (!empty($_GET['password'])) ? $_GET['password'] : NULL;
 
+      //valore default
 
+      $color = 'red';
 
+      $message = 'ACCESS Negato!!';
 
-     ?>
+      // Se la password è valida i valori vengono modificati
 
+      if($password == 'Boolean') {
+
+        $color = 'green';
+        $message = 'ACCESS Consentito!';
+
+      }
+    ?>
+
+        <p class="<?php echo $class; ?>"><?php echo $message; ?></p>
   </body>
 </html>
